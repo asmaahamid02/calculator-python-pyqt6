c544080129f45277fefb158441754151fd9a1211
Asmaa Hamid <asmaahamid002@gmail.com>
2024-06-26 19:36:19 +0300

    upload files

---
+++ README.md
@@ -0,0 +1,86 @@
+# Simple Calculator GUI App
+
+## Description
+
+> This is a simple calculator GUI app implemented using PyQt6 and python with simple arithmetic operations (addition, subtraction, multiplication, division)
+
+## Key concepts
+
+1. Python basics
+2. Class implementation
+3. Inheritance
+4. Package integration
+5. Error handling
+6. Virtual environments
+
+## Technologies
+
+1. [Python3](https://docs.python.org/3/)
+2. [PyQt6](https://www.riverbankcomputing.com/static/Docs/PyQt6/module_index.html): Python binding of the GUI toolkit Qt to implement python GUIs.
+
+## Demo
+
+<!--markdownlint-disable MD033-->
+<img src='./assets/demo.gif' alt='demo' width='500px'>
+
+## How to run
+
+1. Install Python
+
+   - [Python](https://www.python.org/downloads/)
+   - [Python Installation Guide](https://realpython.com/installing-python/)
+
+2. Install pipenv for virtual environment (optional)
+
+   ```bash
+   pip install pipenv
+   ```
+
+3. Create virtual environment (optional)
+
+   ```bash
+   pipenv install
+   pipenv shell
+   ```
+
+   or
+
+   ```bash
+   python -m venv venv
+   ```
+
+4. Install pyqt6
+
+   Enter the virtual environment (optional)
+
+   ```bash
+   ##pipenv
+   pipenv shell
+
+   ##default
+   source venv/Scripts/activate  #linux/mac
+   #or
+   source venv/bin/activate #linux/mac
+   #or
+   venv\Scripts\activate #windows
+   ```
+
+   Install pyqt6
+
+   ```bash
+   #pipenv
+   pipenv install pyqt6
+
+   #pip
+   pip install pyqt6
+   ```
+
+5. Run the project
+
+   ```
+   python calculator.py
+   ```
+
+## Author
+
+> [Asmaa Hamid](https://linktr.ee/asmaahamid02)
---
+++ calculator.py
@@ -0,0 +1,117 @@
+from PyQt6.QtWidgets import QApplication, QPushButton, QWidget, QLineEdit,QVBoxLayout, QMainWindow, QGridLayout, QSizePolicy
+from PyQt6.QtCore import Qt
+import sys
+
+class Calculator(QMainWindow):      
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Calculator")
+        self.setGeometry(100, 100, 400, 450)
+        self.setMinimumHeight(450)
+        self.setMinimumWidth(400)
+
+        self.central_widget = QWidget()
+        self.central_widget.setStyleSheet('background-color: #000000;')
+        self.setCentralWidget(self.central_widget)
+
+        self.app_layout = QVBoxLayout()
+        self.central_widget.setLayout(self.app_layout)
+
+        self.create_input()
+        self.create_buttons()
+
+    def create_input(self):
+        self.input = QLineEdit()
+        self.input.setReadOnly(True)
+        self.input.setAlignment(Qt.AlignmentFlag.AlignRight)
+        self.input.setFixedHeight(100)    
+        self.input.setPlaceholderText('0')
+        self.input.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        self.input.setStyleSheet('font-size: 36px; padding: 10px 5px; font-weight: bold;border:none')
+        self.app_layout.addWidget(self.input)
+
+    def create_buttons(self):    
+        buttons = [
+            'C' , 'Del', '/',
+            '7', '8', '9', '*',
+            '4', '5', '6', '-',
+            '1', '2', '3', '+',
+            '0', '.', '='                    
+        ]            
+
+        grid_layout = QGridLayout()
+
+        row = 0
+        col = 0
+        rowSpan = 1
+        colSpan = 1
+
+        for button_text in buttons:
+            button = QPushButton(button_text)
+            button.setStyleSheet('font-size: 20px; padding: 10px; border-radius: 20px; margin: 3px 2px; background-color: #313131; color: white;')
+
+            if button_text in  ['/', '*', '-', '+', '=']:
+                button.setStyleSheet('background-color: #F78E01; color: white;')
+            elif button_text in ['C', 'Del']:
+                button.setStyleSheet('background-color: #A1A1A1; color: black;')
+                
+            button.clicked.connect(self.click_handler)
+            button.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+
+            if button_text in ['C', '=']:
+                colSpan = 2
+            else:
+                colSpan = 1
+
+            grid_layout.addWidget(button, row, col, rowSpan, colSpan)
+
+            col += colSpan
+
+            if col >= 4:
+                col = 0
+                row += 1
+
+        self.app_layout.addLayout(grid_layout)
+        
+
+    def click_handler(self):
+        button_instance = self.sender()
+        button_text =  button_instance.text()
+        input_text = self.input.text()
+
+        if input_text == 'Error':
+            self.input.clear()
+            input_text = ''
+
+        #validation
+
+        #prevent adding some operators at the beginning
+        if input_text == '' and button_text in ['/', '*', 'C', 'Del', '.', '=', '+']:
+            return
+        
+        #prevent adding multiple operators
+        if button_text in ['/', '*', '-', '+', '=', '.'] and input_text and input_text[-1] in ['/', '*', '-', '+', '.']:
+            return   
+
+        if button_text == '=':
+            try:
+                result = eval(input_text)
+                self.input.setText(str(result))
+            except Exception as error:
+                self.input.setText(f"Error") 
+        elif button_text == 'C':
+            self.input.clear()
+        elif button_text == 'Del':
+           self.input.setText(input_text[:-1])
+        else:
+            self.input.setText(input_text + button_text)   
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    calculator = Calculator()
+    calculator.show()
+
+    sys.exit(app.exec())
+
+
